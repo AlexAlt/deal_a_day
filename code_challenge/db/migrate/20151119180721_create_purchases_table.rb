0572baec20aefb569e2bdf1c412b94575699c4c8
AlexAlt <alexandra.m.altieri@gmail.com>
2015-11-19 15:36:24 -0800

    Change schema so now purchase count is an integer

--- code_challenge/db/migrate/20151119180721_create_purchases_table.rb
+++ code_challenge/db/migrate/20151119180721_create_purchases_table.rb
@@ -4,7 +4,7 @@ class CreatePurchasesTable < ActiveRecord::Migration
       t.string :purchaser_name
       t.string :item_description
       t.decimal :item_price, :precision => 5, :scale => 2
-      t.string :purchase_count
+      t.integer :purchase_count
       t.string :merchant_address
       t.string :merchant_name
       t.integer :file_upload_id
--- code_challenge/db/schema.rb
+++ code_challenge/db/schema.rb
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20151119180721) do
     t.string   "purchaser_name"
     t.string   "item_description"
     t.decimal  "item_price",       precision: 5, scale: 2
-    t.string   "purchase_count"
+    t.integer  "purchase_count"
     t.string   "merchant_address"
     t.string   "merchant_name"
     t.integer  "file_upload_id"
