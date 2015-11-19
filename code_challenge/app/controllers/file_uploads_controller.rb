7d929f2be0e6881d85edbe52548e0ea41dc9dc37
AlexAlt <alexandra.m.altieri@gmail.com>
2015-11-19 10:59:59 -0800

    Write code to pass integration spec.

--- code_challenge/app/controllers/file_uploads_controller.rb
+++ code_challenge/app/controllers/file_uploads_controller.rb
@@ -1,4 +1,11 @@
 class FileUploadsController < ApplicationController
   def index
+    @file_uploads = FileUpload.all
+  end
+
+  def import
+    new_upload = FileUpload.create!
+    Purchase.import(params[:file], new_upload)
+    redirect_to root_path
   end
 end
--- code_challenge/spec/features/upload_path_spec.rb
+++ code_challenge/spec/features/upload_path_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe 'the process for uploading a file' do
   it 'uploads a file' do
     visit '/'
-    attach_file('Import CSV', Rails.root + "../example_data.csv")
+    attach_file('file', Rails.root + "../example_data.csv")
+    click_on "Import CSV"
     expect(page).to have_content 'Purchaser Name'
   end
 end
